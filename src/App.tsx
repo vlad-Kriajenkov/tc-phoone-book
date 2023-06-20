44f6c36fe4e6ce624b22a1cfdf624e2e248542d7
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-20 11:19:24 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Contacts,PhooneBook } from './components/index';
+
+function App() {
+  return (
+    <div>
+      <PhooneBook />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
